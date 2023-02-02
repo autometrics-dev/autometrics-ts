7bac181aff634bc984952cb9f1d36984ca8491f5
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2023-02-02 08:57:17 +0100

    add _autometrics: boolean to the return signature for wrappers and decorators

--- packages/decorators/src/index.ts
+++ packages/decorators/src/index.ts
@@ -1,69 +1,84 @@
-import { OtelMetrics } from "./instrumentation";
-import otel from "@opentelemetry/api"
 import { otelMetrics } from "./instrumentation";
+import otel from "@opentelemetry/api";
 
 /**
-* Autometrics automatically instruments the decorated class method or a wrapped function.
-*
-* Hover over the method/function itself to get the links for your metrics (if you have the language service plugin installed)
-*/
-export default function autometrics(_target: Object, property_key: string, descriptor: PropertyDescriptor) {
-	OtelMetrics.instance() // should ensure the instrumentation is initialized only once no matter how many times this function is called
+ * Autometrics decorator for **class methods** that automatically instruments the decorated method with OpenTelemetry-compatible metrics.
+ *
+ * Hover over the method to get the links for generated queries (if you have the language service plugin installed)
+ */
+export function Autometrics(
+	_target: Object,
+	propertyKey: string,
+	descriptor: PropertyDescriptor
+) {
 	const _meterProvider = otelMetrics;
 	const meter = otel.metrics.getMeter("autometrics-prometheus");
 
-	const original_function = descriptor.value;
+	const originalFunction = descriptor.value;
 
-	descriptor.value = function(...args: any) {
-		let result: any;
-		const autometrics_start = new Date().getTime();
-		const counter = meter.createCounter("method.calls.count")
-		const histogram = meter.createHistogram("method.calls.duration")
-		try {
-			result = original_function.apply(this, args)
-			counter.add(1, { "method": property_key, "result": "ok" })
-			const autometrics_duration = new Date().getTime() - autometrics_start;
-			histogram.record(autometrics_duration, { "method": property_key })
-		} catch (error) {
-			const autometrics_duration = new Date().getTime() - autometrics_start;
-			counter.add(1, { "method": property_key, "result": "error" })
-			histogram.record(autometrics_duration, { "method": property_key })
-		}
-		return result
-	}
+	descriptor.value = Object.assign(
+		function(...args: any) {
+			let result: any;
+			const autometricsStart = new Date().getTime();
+			const counter = meter.createCounter("method.calls.count");
+			const histogram = meter.createHistogram("method.calls.duration");
+			try {
+				result = originalFunction.apply(this, args);
+				counter.add(1, { method: propertyKey, result: "ok" });
+				const autometricsDuration =
+					new Date().getTime() - autometricsStart;
+				histogram.record(autometricsDuration, { method: propertyKey });
+			} catch (error) {
+				const autometricsDuration =
+					new Date().getTime() - autometricsStart;
+				counter.add(1, { method: propertyKey, result: "error" });
+				histogram.record(autometricsDuration, { method: propertyKey });
+			}
+			return result;
+		},
+		{ _autometrics: true }
+	);
 }
 
-type AnyFunction = (...args: any[]) => any
+type AnyFunction = (...args: any[]) => any;
 
 /**
-* Autometrics automatically instruments the wrapped function with OpenTelemetry-compatible metrics.
-* 
-* Hover over the wrapped function to get the links for generated queries (if you have the language service plugin installed)
-*/
-export function autometrics_wrapper<T extends AnyFunction>(fn: T): (...params: Parameters<T>) => ReturnType<T> {
-
-	OtelMetrics.instance() // should ensure the instrumentation is initialized only once no matter how many times this function is called
+ * Autometrics wrapper for **functions** that automatically instruments the wrapped function with OpenTelemetry-compatible metrics.
+ *
+ * Hover over the wrapped function to get the links for generated queries (if you have the language service plugin installed)
+ */
+export function autometrics<T extends AnyFunction>(
+	fn: T
+): (...params: Parameters<T>) => ReturnType<T> & { _autometrics: boolean } {
 	const _meterProvider = otelMetrics;
 	const meter = otel.metrics.getMeter("autometrics-prometheus");
 
-	return function(...params: Parameters<T>): ReturnType<T> {
-		if (fn.name == undefined || fn.name == null) {
-			throw new TypeError("Autometrics decorated function must have a name to succesfully create a metric")
-		}
-		let result: any;
-		const autometrics_start = new Date().getTime();
-		const counter = meter.createCounter("function.calls.count")
-		const histogram = meter.createHistogram("function.calls.duration")
-		try {
-			result = fn(...params);
-			counter.add(1, { "function": fn.name, "result": "ok" })
-			const autometrics_duration = new Date().getTime() - autometrics_start;
-			histogram.record(autometrics_duration, { "function": fn.name })
-		} catch (error) {
-			const autometrics_duration = new Date().getTime() - autometrics_start;
-			counter.add(1, { "method": fn.name, "result": "error" })
-			histogram.record(autometrics_duration, { "method": fn.name })
-		}
-		return result
+	if (fn.name == undefined || fn.name == null) {
+		throw new TypeError(
+			"Autometrics decorated function must have a name to succesfully create a metric"
+		);
 	}
+
+	return Object.assign(
+		function(...params: Parameters<T>): ReturnType<T> {
+			let result: any;
+			const autometricsStart = new Date().getTime();
+			const counter = meter.createCounter("function.calls.count");
+			const histogram = meter.createHistogram("function.calls.duration");
+			try {
+				result = fn(...params);
+				counter.add(1, { function: fn.name, result: "ok" });
+				const autometricsDuration =
+					new Date().getTime() - autometricsStart;
+				histogram.record(autometricsDuration, { function: fn.name });
+			} catch (error) {
+				const autometricsDuration =
+					new Date().getTime() - autometricsStart;
+				counter.add(1, { method: fn.name, result: "error" });
+				histogram.record(autometricsDuration, { method: fn.name });
+			}
+			return result;
+		},
+		{ _autometrics: true }
+	);
 }
