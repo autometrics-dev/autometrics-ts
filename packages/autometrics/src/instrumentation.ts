568bc80b399350b7043984ce24c3bad5d360cff4
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2023-02-10 14:57:03 +0100

    refactor instrumentalization initializer

--- packages/autometrics/src/instrumentation.ts
+++ packages/autometrics/src/instrumentation.ts
@@ -2,15 +2,22 @@ import otel from "@opentelemetry/api";
 import { MeterProvider } from "@opentelemetry/sdk-metrics";
 import { PrometheusExporter } from "@opentelemetry/exporter-prometheus";
 
+let initialized: boolean;
+
 /**
  * This initializes the OpenTelemetry meter and sets up the Prometheus exporter
  */
-
-export const OTEL_METRICS = (function () {
+export function initializeMetrics() {
   const exporter = new PrometheusExporter();
-  const autometricsMeterProvider = new MeterProvider();
-  autometricsMeterProvider.addMetricReader(exporter);
+  if (initialized) {
+    return;
+  } else {
+    const autometricsMeterProvider = new MeterProvider();
+    autometricsMeterProvider.addMetricReader(exporter);
 
-  otel.metrics.setGlobalMeterProvider(autometricsMeterProvider);
-  return autometricsMeterProvider;
-})();
+    otel.metrics.setGlobalMeterProvider(autometricsMeterProvider);
+    initialized = true;
+  }
+  return;
+}
+
