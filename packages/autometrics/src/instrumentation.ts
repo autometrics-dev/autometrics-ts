3c02131851b358a16bb5128a609096e33ca5276a
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2023-02-13 20:18:54 +0100

    rewrite instrumentation initializer

--- packages/autometrics/src/instrumentation.ts
+++ packages/autometrics/src/instrumentation.ts
@@ -1,26 +1,24 @@
 import otel from "@opentelemetry/api";
-import { MeterProvider } from "@opentelemetry/sdk-metrics";
+import { MeterProvider, MetricReader } from "@opentelemetry/sdk-metrics";
 import { PrometheusExporter } from "@opentelemetry/exporter-prometheus";
 
-let initialized: boolean;
+let initialized = false;
+
+export interface AutometricsConfig {
+  exporter?: MetricReader;
+}
 
 /**
  * This initializes the OpenTelemetry meter and sets up the Prometheus exporter
  */
-export const initializeMetrics = (() => {
-  let autometricsMeterProvider: MeterProvider;
+export function initializeMetrics(config?: AutometricsConfig) {
+  if (initialized) {
+    return; // we return early if the metrics exporter is already initialized 
+  }
 
-  return () => {
-    if (!autometricsMeterProvider) {
-      autometricsMeterProvider = new MeterProvider();
-      const exporter = new PrometheusExporter();
-      autometricsMeterProvider.addMetricReader(exporter);
-      otel.metrics.setGlobalMeterProvider(autometricsMeterProvider);
-
-      return autometricsMeterProvider
-    } else {
-      return autometricsMeterProvider;
-    }
-  };
-
-})()
+  initialized = true;
+  const exporter = config?.exporter || new PrometheusExporter();
+  const autometricsMeterProvider = new MeterProvider();
+  autometricsMeterProvider.addMetricReader(exporter);
+  otel.metrics.setGlobalMeterProvider(autometricsMeterProvider);
+}
