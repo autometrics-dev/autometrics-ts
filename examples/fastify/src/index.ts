c66cf6d926d987f87fae512fc39efc8576910e64
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2023-02-20 11:47:52 +0100

    fix tabs to spaces

--- examples/fastify/src/index.ts
+++ examples/fastify/src/index.ts
@@ -1,8 +1,8 @@
 import {
-	fastify,
-	FastifyReply,
-	FastifyRequest,
-	RouteShorthandOptions,
+  fastify,
+  FastifyReply,
+  FastifyRequest,
+  RouteShorthandOptions,
 } from "fastify";
 import { autometrics } from "@autometrics/autometrics";
 
@@ -10,34 +10,34 @@ const port = 7000;
 const server = fastify();
 
 const opts: RouteShorthandOptions = {
-	schema: {
-		response: {
-			200: {
-				type: "object",
-				properties: {
-					body: {
-						type: "string",
-					},
-				},
-			},
-		},
-	},
+  schema: {
+    response: {
+      200: {
+        type: "object",
+        properties: {
+          body: {
+            type: "string",
+          },
+        },
+      },
+    },
+  },
 };
 
 async function rootHandler(req: FastifyRequest, response: FastifyReply) {
-	return { body: "Hello world" };
+  return { body: "Hello world" };
 }
 
 server.get("/", opts, autometrics(rootHandler));
 
 const start = async () => {
-	try {
-		await server.listen({ port: port });
-		console.log(`Server started successfully on: ${port}`);
-	} catch (err) {
-		server.log.error(err);
-		process.exit(1);
-	}
+  try {
+    await server.listen({ port: port });
+    console.log(`Server started successfully on: ${port}`);
+  } catch (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
 };
 
 start();
--- packages/autometrics/src/instrumentation.test.ts
+++ packages/autometrics/src/instrumentation.test.ts
@@ -1,10 +1,8 @@
 import { getMetricsProvider } from "./instrumentation";
-import { describe, test, expect, } from "vitest";
+import { describe, test, expect } from "vitest";
 
 describe("Autometrics initializer", () => {
-
   test("Test if autometrics initializes only once", () => {
-		expect(getMetricsProvider()).to.equal(getMetricsProvider())
+    expect(getMetricsProvider()).to.equal(getMetricsProvider());
   });
 });
-
