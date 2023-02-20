ae3a6bb6c4d6ea4ab217b753860ced161d08b63e
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2023-02-20 10:30:25 +0100

    get express example to use the new API

--- examples/express/src/index.ts
+++ examples/express/src/index.ts
@@ -1,6 +1,6 @@
 // This is an example that makes use of the setMetricsExporter API to allow user to configure the exporter
 import express from "express";
-import { autometrics, setMetricsExporter } from "autometrics";
+import { autometrics, setMetricsExporter } from "@autometrics/autometrics";
 import { PrometheusExporter } from "@opentelemetry/exporter-prometheus";
 
 const exporter = new PrometheusExporter({port: 7777})
