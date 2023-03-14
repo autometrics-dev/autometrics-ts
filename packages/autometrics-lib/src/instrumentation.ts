4b7d48512ffd2319ffbfd9a412adb62297c0d6f6
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2023-03-14 11:27:27 +0100

    other instrumentation.ts cleanup

--- packages/autometrics-lib/src/instrumentation.ts
+++ packages/autometrics-lib/src/instrumentation.ts
@@ -69,22 +69,20 @@ export function getMeter(meter = "autometrics-prometheus") {
   return getMetricsProvider().getMeter(meter);
 }
 
-function logger(msg: string) {
-  console.log(`Autometrics: ${msg}`);
-}
-
 async function pushToGateway(gateway: string) {
   const exporterResponse = await exporter.collect();
   const metrics = exporterResponse.resourceMetrics;
   const serialized = new PrometheusSerializer().serialize(metrics);
 
-  const res = await fetch(`http://${gateway}/metrics/`, {
+  await fetch(`${gateway}/metrics`, {
     method: "POST",
     mode: "cors",
     body: serialized,
   });
 
-  console.log(res);
-
   await exporter.forceFlush();
+}
+
+function logger(msg: string) {
+  console.log(`Autometrics: ${msg}`);
 }
