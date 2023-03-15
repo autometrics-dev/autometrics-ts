aac29b4fd4aad541130dab666a21911beded7b36
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2023-03-15 11:20:08 +0100

    update pushToGateway

--- packages/autometrics-lib/src/instrumentation.ts
+++ packages/autometrics-lib/src/instrumentation.ts
@@ -44,6 +44,21 @@ export function init(options: initOptions) {
   }
 }
 
+async function pushToGateway(gateway: string) {
+  const exporterResponse = await exporter.collect();
+  const serialized = new PrometheusSerializer().serialize(
+    exporterResponse.resourceMetrics,
+  );
+
+  await fetch(gateway, {
+    method: "POST",
+    mode: "cors",
+    body: serialized,
+  });
+  // we flush the metrics at the end of the submission to ensure the data is not repeated
+  await exporter.forceFlush();
+}
+
 /**
  * Instantiates an autometrics meter provider and default Prometheus exporter
  * if none exist)
@@ -69,20 +84,6 @@ export function getMetricsProvider() {
  */
 export function getMeter(meter = "autometrics-prometheus") {
   return getMetricsProvider().getMeter(meter);
-}
-
-async function pushToGateway(gateway: string) {
-  const exporterResponse = await exporter.collect();
-  const metrics = exporterResponse.resourceMetrics;
-  const serialized = new PrometheusSerializer().serialize(metrics);
-
-  await fetch(`${gateway}/metrics`, {
-    method: "POST",
-    mode: "cors",
-    body: serialized,
-  });
-
-  await exporter.forceFlush();
 }
 
 function logger(msg: string) {
