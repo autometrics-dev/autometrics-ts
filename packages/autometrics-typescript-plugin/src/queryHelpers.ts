1802001cfb0ac29ebe4b21f87bc1d652281f7776
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2023-05-11 10:38:40 +0200

    fix formatting

--- packages/autometrics-typescript-plugin/src/queryHelpers.ts
+++ packages/autometrics-typescript-plugin/src/queryHelpers.ts
@@ -8,15 +8,11 @@ export function createLatencyQuery(nodeIdentifier: string) {
   return `histogram_quantile(0.99, ${latency}) or histogram_quantile(0.95, ${latency})`;
 }
 
-export function createRequestRateQuery(
-  nodeIdentifier: string,
-) {
+export function createRequestRateQuery(nodeIdentifier: string) {
   return `sum by (function, module, commit, version) (rate(function_calls_count_total{function="${nodeIdentifier}"}[5m]) ${BUILD_INFO_LABELS})`;
 }
 
-export function createErrorRatioQuery(
-  nodeIdentifier: string,
-) {
+export function createErrorRatioQuery(nodeIdentifier: string) {
   const requestQuery = createRequestRateQuery(nodeIdentifier);
   return `sum by (function, module, commit, version) (rate(function_calls_count_total{function="${nodeIdentifier}",result="error"}[5m]) ${BUILD_INFO_LABELS}) / ${requestQuery}`;
 }
