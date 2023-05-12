0a64e3baec33d52f3e8b82a5f80e50111a3189ad
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2023-05-12 11:55:40 +0200

    remove redundant elses

--- packages/autometrics-lib/src/buildInfo.ts
+++ packages/autometrics-lib/src/buildInfo.ts
@@ -11,24 +11,24 @@ let buildInfo: BuildInfo | undefined;
 export function registerBuildInfo() {
   if (buildInfo) {
     return;
-  } else {
-    buildInfo = {
-      version: getVersion(),
-      commit: getCommit(),
-      branch: getBranch(),
-    };
-
-    const gauge = getMeter().createUpDownCounter("build_info");
-    gauge.add(1, buildInfo);
   }
+
+  buildInfo = {
+    version: getVersion(),
+    commit: getCommit(),
+    branch: getBranch(),
+  };
+
+  const gauge = getMeter().createUpDownCounter("build_info");
+  gauge.add(1, buildInfo);
 }
 
 function getVersion() {
   if (process.env.npm_package_version) {
     return process.env.npm_package_version;
-  } else {
-    return process.env.PACKAGE_VERSION || process.env.AUTOMETRICS_VERSION;
   }
+
+  return process.env.PACKAGE_VERSION || process.env.AUTOMETRICS_VERSION;
 }
 
 function getCommit() {
