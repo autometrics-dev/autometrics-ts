e289ff753cdaf4cee43dad975bfde7ee95553c5b
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2023-05-24 22:07:07 +0200

    buildInfo auxiliaries were breaking the library in browser - this guards for it

--- packages/lib/src/buildInfo.ts
+++ packages/lib/src/buildInfo.ts
@@ -21,20 +21,47 @@ export function registerBuildInfo() {
 
   const gauge = getMeter().createUpDownCounter("build_info");
   gauge.add(1, buildInfo);
+
+	return buildInfo;
 }
 
-function getVersion() {
-  if (process.env.npm_package_version) {
-    return process.env.npm_package_version;
+function getVersion(): string | undefined {
+  if (process) {
+    if (process.env.npm_package_version) {
+      return process.env.npm_package_version;
+    }
+    return process.env.PACKAGE_VERSION || process.env.AUTOMETRICS_VERSION;
+    //@ts-ignore
+  } else if (Deno) {
+    return (
+      //@ts-ignore
+      Deno.env.get("AUTOMETRICS_VERSION") || Deno.env.get("PACKAGE_VERSION")
+    );
+  } else {
+    return;
   }
-
-  return process.env.PACKAGE_VERSION || process.env.AUTOMETRICS_VERSION;
 }
 
 function getCommit() {
-  return process.env.COMMIT_SHA || process.env.AUTOMETRICS_COMMIT;
+  if (process) {
+    return process.env.COMMIT_SHA || process.env.AUTOMETRICS_COMMIT;
+    //@ts-ignore
+  } else if (Deno) {
+    //@ts-ignore
+    return Deno.env.get("AUTOMETRICS_COMMIT");
+  } else {
+    return;
+  }
 }
 
 function getBranch() {
-  return process.env.BRANCH_NAME || process.env.AUTOMETRICS_BRANCH;
+  if (process) {
+    return process.env.BRANCH_NAME || process.env.AUTOMETRICS_BRANCH;
+    //@ts-ignore
+  } else if (Deno) {
+    //@ts-ignore
+    return Deno.env.get("AUTOMETRICS_BRANCH");
+  } else {
+    return;
+  }
 }
