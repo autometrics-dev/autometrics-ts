3a71d901d3bd6059854fdd6c93938045aba02361
Stephan Lagerwaard <stephanlagerwaard@gmail.com>
2023-05-26 10:43:01 +0200

    Add test for autometrics in a variable assigment

--- packages/parcel-transformer-autometrics/src/lib.test.ts
+++ packages/parcel-transformer-autometrics/src/lib.test.ts
@@ -18,13 +18,9 @@ autometrics({ functionName: "myFunction", moduleName: "module.ts" }, function my
   test("Adds moduleName without updating functionName", () => {
     const filePath = "path/to/module2.ts";
 
-    const source = `
-autometrics({ functionName: "renamedFunction" }, function myFunction() { });
-  `.trim();
+    const source = `autometrics({ functionName: "renamedFunction" }, function myFunction() { });`;
 
-    const transformed = `
-autometrics({ functionName: "renamedFunction", moduleName: "module2.ts" }, function myFunction() { });
-  `.trim();
+    const transformed = `autometrics({ functionName: "renamedFunction", moduleName: "module2.ts" }, function myFunction() { });`;
 
     expect(addAutometricsOptions(source, filePath)).toEqual(transformed);
   });
@@ -32,13 +28,19 @@ autometrics({ functionName: "renamedFunction", moduleName: "module2.ts" }, funct
   test("Adds functionName without updating moduleName", () => {
     const filePath = "path/to/module3.ts";
 
-    const source = `
-autometrics({ moduleName: "renamedModule.ts" }, function myFunction() { });
-  `.trim();
+    const source = `autometrics({ moduleName: "renamedModule.ts" }, function myFunction() { });`;
 
-    const transformed = `
-autometrics({ moduleName: "renamedModule.ts", functionName: "myFunction" }, function myFunction() { });
-  `.trim();
+    const transformed = `autometrics({ moduleName: "renamedModule.ts", functionName: "myFunction" }, function myFunction() { });`;
+
+    expect(addAutometricsOptions(source, filePath)).toEqual(transformed);
+  });
+
+  test("Adds autometrics options to a wrapped function in a variable assignment", () => {
+    const filePath = "path/to/module4.ts";
+
+    const source = `const myFunction = autometrics(function myFunction() { });`;
+
+    const transformed = `const myFunction = autometrics({ moduleName: "module4.ts", functionName: "myFunction" }, function myFunction() { });`;
 
     expect(addAutometricsOptions(source, filePath)).toEqual(transformed);
   });
