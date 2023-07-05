147eda1f7a5024c033275648e83b3475188e7c34
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2023-07-05 14:46:19 +0200

    add a better express example

---
+++ examples/express/src/database.ts
@@ -0,0 +1,54 @@
+import { delayRandomDuration, shouldError } from "./util.js";
+import { Autometrics } from "@autometrics/autometrics";
+
+interface User {
+  id: number;
+  name: string;
+}
+
+@Autometrics()
+export class DatabaseClient {
+  public users: User[];
+  constructor() {
+    this.users = [
+      {
+        id: 1,
+        name: "John",
+      },
+      {
+        id: 2,
+        name: "Jane",
+      },
+    ];
+  }
+
+  public async getUsers() {
+    shouldError();
+    await delayRandomDuration();
+    return this.users;
+  }
+
+  public async getUser(id: number) {
+    shouldError();
+    await delayRandomDuration();
+    return this.users.find((user) => user.id === id);
+  }
+
+  public async addUser(user: User) {
+    shouldError();
+    await delayRandomDuration();
+    this.users.push(user);
+    return user;
+  }
+
+  public async deleteUser(id: number) {
+    shouldError();
+    await delayRandomDuration();
+    const user = this.users.find((user) => user.id === id);
+    if (!user) {
+      throw new Error("User not found");
+    }
+    this.users = this.users.filter((user) => user.id !== id);
+    return user;
+  }
+}
---
+++ examples/express/src/routes.ts
@@ -0,0 +1,45 @@
+import express from "express";
+import { db } from "./server.js";
+
+export async function handleGetUsers(req: express.Request, res: express.Response) {
+  try {
+    const users = await db.getUsers();
+    return res.status(200).json(users);
+  } catch (e) {
+    return res.status(500).send(e.message);
+  }
+}
+
+export async function handleGetUserById(req: express.Request, res: express.Response) {
+  const id = parseInt(req.params.id);
+  try {
+    const user = await db.getUser(id);
+
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+
+    return res.status(200).json(user);
+  } catch (e) {
+    return res.status(500).send(e.message);
+  }
+}
+
+export async function handleCreateUser(req: express.Request, res: express.Response) {
+  try {
+    const user = await db.addUser(req.body);
+    return res.status(201).json(user);
+  } catch (e) {
+    return res.status(500).send(e.message);
+  }
+}
+
+export async function handleDeleteUser(req: express.Request, res: express.Response) {
+  const id = parseInt(req.params.id);
+  try {
+    const user = await db.deleteUser(id);
+    return res.status(200).json(user);
+  } catch (e) {
+    return res.status(500).send(e.message);
+  }
+}
