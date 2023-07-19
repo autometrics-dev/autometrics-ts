2c470965ab8c4c2d2b39a99b5e617c811b626c47
Brett Beutell <brett@fiberplane.com>
2023-07-19 23:07:36 +0200

    Update faas example a lil bit

--- examples/faas-experimental/index.ts
+++ examples/faas-experimental/index.ts
@@ -1,6 +1,14 @@
 /**
  * This example should illustrate how you would use autometrics in a FaaS environment.
- * 
+ *
+ * If you need an aggregation gateway locally, try:
+ *
+    docker pull ghcr.io/zapier/prom-aggregation-gateway:v0.7.1
+    docker run --platform linux/amd64 \
+      -dit --name aggregation-gateway \
+      -p 9092:80 \
+      ghcr.io/zapier/prom-aggregation-gateway:v0.7.1
+ *
  */
 
 // NOTE - for now we need a fetch polyfill
@@ -12,16 +20,15 @@ initAutometrics({
   // NOTE - The current default exporter does not play nicely with Prometheus Push Gateway,
   //        You'll end up with the error: "pushed metrics are invalid or inconsistent with existing metrics: pushed metrics must not have timestamps"
   //        However, it does work with aggregation gateways
-  // pushGateway: "http://localhost:9091/pushgateway/metrics/job/kaas/instance/hi-there",
+  //
   pushGateway: "http://localhost:9092/metrics",
   pushInterval: 0,
 });
 
 // Create a getCheese function that returns "gouda"
-const getGouda = autometrics( async function getGouda() {
+const getGouda = autometrics(async function getGouda() {
   await new Promise((resolve) => setTimeout(resolve, 120));
   return "gouda";
 });
 
 getGouda();
-
