efc77586973c661b7266989c118f57e210aff805
Brett Beutell <brett@fiberplane.com>
2023-07-21 13:00:44 +0200

    Reformat

--- packages/lib/src/instrumentation.ts
+++ packages/lib/src/instrumentation.ts
@@ -190,21 +190,8 @@ export function getMetricsProvider() {
         new View({
           // See: https://github.com/autometrics-dev/autometrics-ts/issues/102
           aggregation: new ExplicitBucketHistogramAggregation([
-            0,
-            0.005,
-            0.01,
-            0.025,
-            0.05,
-            0.075,
-            0.1,
-            0.25,
-            0.5,
-            0.75,
-            1,
-            2.5,
-            5,
-            7.5,
-            10,
+            0, 0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5,
+            7.5, 10,
           ]),
           instrumentName: HISTOGRAM_NAME,
         }),
