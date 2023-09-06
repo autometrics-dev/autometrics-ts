b2c767a42fc4f4c801455a52c93cc3823ba7a10e
Arend van Beelen jr <arendjr@gmail.com>
2023-09-06 12:20:56 +0200

    Drop on-demand-metric-reader package (#117)
    * Drop on-demand-metric-reader package
    
    * Fix dependencies

---
+++ packages/exporter-otlp-http/tests/temporality.test.ts
@@ -0,0 +1,53 @@
+import { AggregationTemporality } from "@opentelemetry/sdk-metrics";
+import { autometrics } from "@autometrics/autometrics";
+import { describe, expect, test } from "vitest";
+
+import { COUNTER_NAME } from "../../autometrics/src/constants";
+import { init } from "../src/index";
+import { metricReader } from "../src/registerExporterInternal";
+
+const foo = autometrics(function foo() {});
+
+describe("temporality test with scheduled push", () => {
+  test("clears metrics when they are pushed and DELTA temporality is used", async () => {
+    const pushInterval = 50;
+    const timeout = 10; // must be smaller than `pushInterval`.
+
+    init({
+      url: "/metrics",
+      pushInterval,
+      timeout,
+      temporalityPreference: AggregationTemporality.DELTA,
+    });
+
+    foo(); // record metric
+
+    const collectionResultBeforePush = await metricReader?.collect();
+
+    const counterMetricBeforePush =
+      collectionResultBeforePush?.resourceMetrics.scopeMetrics[0].metrics.find(
+        (metric) => metric.descriptor.name === COUNTER_NAME,
+      );
+
+    if (!counterMetricBeforePush) {
+      throw new Error("Counter metric not recorded");
+    }
+
+    expect(counterMetricBeforePush.dataPoints[0].value).toBe(1);
+
+    await new Promise((resolve) => setTimeout(resolve, pushInterval));
+
+    const collectionResultAfterPush = await metricReader?.collect();
+
+    const counterMetricAfterPush =
+      collectionResultAfterPush?.resourceMetrics.scopeMetrics[0].metrics.find(
+        (metric) => metric.descriptor.name === COUNTER_NAME,
+      );
+
+    if (!counterMetricAfterPush) {
+      throw new Error("Counter metric not recorded");
+    }
+
+    expect(counterMetricAfterPush.dataPoints.length).toBe(0);
+  });
+});
--- packages/on-demand-metric-reader/README.md
+++
@@ -1,7 +0,0 @@
-# `@autometrics/on-demand-metric-reader`
-
-An on-demand metric reader for OpenTelemetry-js.
-
-This can be used as an alternative for OpenTelemetry-js's
-`PeriodicExportingMetricReader` for use cases where periodic scheduling is less
-feasible and on-demand pushing of metrics is desired.
