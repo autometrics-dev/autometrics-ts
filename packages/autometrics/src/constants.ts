16d692637ab380355a74185e0c0febb091bc17be
Arend van Beelen jr <arendjr@gmail.com>
2023-11-13 15:03:29 +0100

    Use dots for OTLP labels (#140)

--- packages/autometrics/src/constants.ts
+++ packages/autometrics/src/constants.ts
@@ -11,21 +11,21 @@ export const GAUGE_NAME = "function.calls.concurrent" as const;
 export const BUILD_INFO_NAME = "build_info" as const;
 
 // Labels
-export const AUTOMETRICS_VERSION_LABEL = "autometrics_version" as const;
+export const AUTOMETRICS_VERSION_LABEL = "autometrics.version" as const;
 export const BRANCH_LABEL = "branch" as const;
-export const CALLER_FUNCTION_LABEL = "caller_function" as const;
-export const CALLER_MODULE_LABEL = "caller_module" as const;
+export const CALLER_FUNCTION_LABEL = "caller.function" as const;
+export const CALLER_MODULE_LABEL = "caller.module" as const;
 export const COMMIT_LABEL = "commit" as const;
 export const FUNCTION_LABEL = "function" as const;
 export const MODULE_LABEL = "module" as const;
-export const OBJECTIVE_NAME_LABEL = "objective_name" as const;
-export const OBJECTIVE_PERCENTILE_LABEL = "objective_percentile" as const;
+export const OBJECTIVE_NAME_LABEL = "objective.name" as const;
+export const OBJECTIVE_PERCENTILE_LABEL = "objective.percentile" as const;
 export const OBJECTIVE_LATENCY_THRESHOLD_LABEL =
-  "objective_latency_threshold" as const;
-export const REPOSITORY_URL_LABEL = "repository_url" as const;
-export const REPOSITORY_PROVIDER_LABEL = "repository_provider" as const;
+  "objective.latency_threshold" as const;
+export const REPOSITORY_URL_LABEL = "repository.url" as const;
+export const REPOSITORY_PROVIDER_LABEL = "repository.provider" as const;
 export const RESULT_LABEL = "result" as const;
-export const SERVICE_NAME_LABEL = "service_name" as const;
+export const SERVICE_NAME_LABEL = "service.name" as const;
 export const VERSION_LABEL = "version" as const;
 
 // Descriptions
