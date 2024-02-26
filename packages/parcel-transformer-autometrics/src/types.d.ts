3655aee1e3c7b917e74094a33fa5ab6619d860f1
Brett Beutell <brbeut@gmail.com>
2024-02-26 17:08:46 +0100

    HACK - fix type issues with parcel-transformer when building

--- packages/parcel-transformer-autometrics/src/types.d.ts
+++ packages/parcel-transformer-autometrics/src/types.d.ts
@@ -1,3 +1,10 @@
 declare module "@parcel/profiler" {
   type TraceMeasurement = {};
 }
+
+declare module "@parcel/watcher" {
+  type Event = {};
+  type EventType = {};
+  type AsyncSubscription = {};
+  type Options = {};
+}
